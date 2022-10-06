28761378aaf18ef411234a2cbf0cb252396cb9c3
Giuseppel99 <80783925+Giuseppel99@users.noreply.github.com>
2022-10-06 13:13:38 +0200

    finding button connected to google maps

--- app/src/main/java/com/unipv/pacifinder/MainActivity.java
+++ app/src/main/java/com/unipv/pacifinder/MainActivity.java
@@ -2,7 +2,14 @@ package com.unipv.pacifinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton findButton = findViewById(R.id.findButton);
+
+        findButton.setOnClickListener(view -> {
+
+            Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4194");
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+            mapIntent.setPackage("com.google.android.apps.maps");
+            if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                startActivity(mapIntent);
+
+                Context context = getApplicationContext();
+                CharSequence text = "Ongoing Research...";
+                int duration = Toast.LENGTH_LONG;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
     }
 }
