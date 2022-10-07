71a90f67331407439e6024f5f5d6db7d64cff372
Giuseppel99 <80783925+Giuseppel99@users.noreply.github.com>
2022-10-07 17:49:56 +0200

    added toolbar with setting and profile icon

--- app/src/main/java/com/unipv/pacifinder/MainActivity.java
+++ app/src/main/java/com/unipv/pacifinder/MainActivity.java
@@ -2,12 +2,12 @@ package com.unipv.pacifinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +16,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initFindButton();
+        }
+
+        private void initFindButton(){
 
         ImageButton findButton = findViewById(R.id.findButton);
 
@@ -24,16 +28,14 @@ public class MainActivity extends AppCompatActivity {
             Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4194");
             Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
             mapIntent.setPackage("com.google.android.apps.maps");
+
             if (mapIntent.resolveActivity(getPackageManager()) != null) {
-                startActivity(mapIntent);
 
-                Context context = getApplicationContext();
                 CharSequence text = "Ongoing Research...";
-                int duration = Toast.LENGTH_LONG;
-
-                Toast toast = Toast.makeText(context, text, duration);
+                Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG);
                 toast.show();
+                startActivity(mapIntent);
             }
         });
-        }
+    }
 }
